fac98b5bc3ddc3e414af23ff258b0b50517420a2
not ervin <notminin@pm.me>
2019-04-14 18:45:57 +0100

    wrap

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const util = require("util");
 const glob = util.promisify(require("glob"));
 
-glob("**/*.m.css")
+getSettings()
 	.then(console.log)
 	.catch(console.error);
+
+function getSettings() {
+	return new Promise((resolve, reject) => {
+		glob("**/*.m.css")
+			.then(resolve)
+			.catch(reject);
+	});
+}
