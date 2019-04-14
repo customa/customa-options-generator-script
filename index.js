917385ef20e92342da740daaa78e437d1e9eb847
not ervin <notminin@pm.me>
2019-04-14 21:29:13 +0100

    document code

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ getSettings()
 
 function getSettings() {
 	return new Promise((resolve, reject) => {
-		let current = fs.realpathSync(".");
-
-		glob("**/*.m.css").then((files) => {
-			// create module and 
+		// get all module files
+		glob("*/*.m.css").then((files) => {
+			// create category tree
 			let data = {};
 
+			// for each module found
 			files.forEach((path) => {
+				// get category and module from path of module
 				let category = path.split("/")[0];
 				let module = path.split("/")[1];
 
@@ -24,39 +25,52 @@ function getSettings() {
 				data[category].push(module);
 			});
 
+			// get all categories
 			Object.keys(data).forEach((category) => {
 				let modules = data[category];
 
 				let newCategory = {};
 
+				// get all modules in a category
 				modules.forEach((module) => {
+					// read module file
 					let content = fs.readFileSync(`${category}/${module}`, "UTF-8").split(/\r\n|\n/);
 
 					let variables = {};
 
+					// loop over each line
 					for (let i = 0; i < content.length; i++) {
 						let line = content[i];
 
+						// save time by only looping through :root
+						// :root should be at start of file so the first closing bracket belongs to it
 						if (line == "}")
 							break;
 
+						// ignore internal variables (settings module, variables module)
+						// ignore code block language variables (holy shit there are so many, and no need to configure)
 						if (/[ |\t]*--INT.*;/.test(line) || /[ |\t]*--ContentProgLang-.*;/.test(line))
 							continue;
 
+						// make ***sure*** line is variable
 						if (/[ |\t]*--.*(\*\/|;)/.test(line)) {
+							// get name and value of variable
 							let name = /[ |\t]*--(.*):/g.exec(line);
 							let value = /[ |\t]*--.*: *(.*);/gm.exec(line);
 
 							if (!name || !value)
 								continue;
 
-							variables[name[1]] = value[1]
+							// assign
+							variables[name[1]] = value[1];
 						}
 					}
 
+					// assign
 					newCategory[module] = variables;
 				});
 
+				// replace list of modules with object of modules and values
 				data[category] = newCategory;
 			});
 
